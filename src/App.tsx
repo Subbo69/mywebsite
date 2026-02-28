aaa71ea5feb80d0b1467e3b5699a562d82f76bfb
Subbo69 <subspeedytk@gmail.com>
2026-02-28 15:48:20 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import WhyUs from './components/WhyUs';
 import WorkWithUs from './components/WorkWithUs';
 import BookingModal from './components/BookingModal';
 import ChatBot from './components/ChatBot';
-import GlobalParticles from './components/GlobalParticles'; // Ensure this file exists!
 
 function App() {
   const [isBookingOpen, setIsBookingOpen] = useState(false);
@@ -19,55 +18,36 @@ function App() {
   };
 
   return (
-    // "relative" is required here so children can use "z-index"
-    <div className="relative min-h-screen bg-white">
-      
-      {/* 1. THE BACKGROUND LAYER (Fixed) */}
-      <GlobalParticles />
-
-      {/* 2. THE CONTENT LAYER (Sits on top of animation) */}
-      <div className="relative z-10">
-        <Header
-          onBookingClick={() => setIsBookingOpen(true)}
-          language={language}
-          onLanguageChange={setLanguage}
-        />
-        
-        <Hero
-          onBookingClick={() => setIsBookingOpen(true)}
-          onAskAIClick={() => openChatWithContext('general')}
-          language={language}
-        />
-        
-        <Services
-          onAskAIClick={(context: string) => openChatWithContext(context)}
-          language={language}
-        />
-        
-        <Testimonials />
-        
-        <WhyUs language={language} />
-        
-        <WorkWithUs
-          onBookingClick={() => setIsBookingOpen(true)}
-          language={language}
-        />
-      </div>
-
-      {/* 3. MODALS & UI OVERLAYS */}
+    <div className="min-h-screen">
+      <Header
+        onBookingClick={() => setIsBookingOpen(true)}
+        language={language}
+        onLanguageChange={setLanguage}
+      />
+      <Hero
+        onBookingClick={() => setIsBookingOpen(true)}
+        onAskAIClick={() => openChatWithContext('general')}
+        language={language}
+      />
+      <Services
+        onAskAIClick={(context: string) => openChatWithContext(context)}
+        language={language}
+      />
+      <Testimonials />
+      <WhyUs language={language} />
+      <WorkWithUs
+        onBookingClick={() => setIsBookingOpen(true)}
+        language={language}
+      />
       <BookingModal
         isOpen={isBookingOpen}
         onClose={() => setIsBookingOpen(false)}
         language={language}
       />
-      
-      <ChatBot 
-        context={chatContext} 
-        onContextUsed={() => setChatContext('')} 
-        language={language} 
-      />
+      <ChatBot context={chatContext} onContextUsed={() => setChatContext('')} language={language} />
     </div>
   );
 }
 
 export default App;
+
