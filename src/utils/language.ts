2169795afd32ea48ca486d2893e5c35628d2957c
Subbo69 <subspeedytk@gmail.com>
2026-03-07 13:21:43 +0100

    Update language.ts

--- src/utils/language.ts
+++ src/utils/language.ts
@@ -1,1 +1,11 @@
+export const detectLanguage = () => {
+  const browserLang = navigator.language.split("-")[0]
 
+  const supported = ["en", "es", "fr", "de"]
+
+  if (supported.includes(browserLang)) {
+    return browserLang
+  }
+
+  return "en"
+}
